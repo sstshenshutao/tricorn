e9b2c664bb8dd9cc4b15b4531c9077064bfe0c46
Shutao Shen <sstbage@gmail.com>
2020-01-08 20:21:33 +0100

    fin tricorn.c for assembly code

--- Implementierung/tricorn.c
+++ Implementierung/tricorn.c
@@ -1,12 +1,18 @@
+//todos: finish all the todos, check free, check under_score_case
 #include <stdio.h>
 #include <getopt.h>
 #include <string.h>
 #include <stdlib.h>
-// extern void multicorn(float r_start, float r_end, float i_start, float i_end, float res, unsigned char *img);
+extern void multicorn(float r_start, float r_end, float i_start, float i_end, float res, unsigned char *img);
 void parse_arg(int argc, char **argv);
 void print_manual();
 int check_error(int *flags, struct option opts[]);
 int safe_strtof(float *parameter, char *optarg);
+#define A_START -2
+#define A_END 1
+#define B_START -1
+#define B_END 1
+
 // this is the entry of the program
 int main(int argc, char **argv)
 {
@@ -86,7 +92,59 @@ void parse_arg(int argc, char **argv)
     printf("i_end:%.3f\n", i_end);
     printf("res:%.3f\n", res);
     printf("output:%s\n", output);
+    // malloc the space for the img buffer.
+    unsigned char *img;
+    int block_len= calculate_block_len(res);
+    malloc_image_buffer(block_len,img);
+    multicorn(r_start,r_end,i_start,i_end,res,img);
+    write_out_file(block_len,img,output);
+    free(img);
     free(flags);
+}
+int calculate_block_len(float res){
+    // float a_len = A_END-A_START;
+    // float b_len = B_END-B_START;
+    // int width=(int)(a_len/res)+1;
+    // int height= (int)(b_len/res)+1;
+    // return width*height;
+    return 500000;
+}
+void malloc_image_buffer(int block_len, unsigned char *img){
+    // todo:need to handle the case: 3/res=0.4 is not divisible later
+    img= malloc(block_len*3*sizeof(unsigned char));
+    if (img == NULL)
+    {
+        printf("Memory not allocated.\n");
+        exit(1);
+    }
+}
+void write_out_file(int block_len,unsigned char *img,char* output_path){
+    unsigned char header[54]={0x42,0x4d,0x96,0xe3,0x16,0x00,0x00,0x00,0x00,0x00,0x36,0x00,0x00,0x00,0x28,0x00,0x00,0x00,0xe8,0x03,0x00,0x00,0xf4,0x01,0x00,0x00,0x01,0x00,0x18,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x12,0x0b,0x00,0x00,0x12,0x0b,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
+    int block_size = block_len*3+54;
+    if (block_size>0xffffffff){
+        printf("file is too big to save in .bmp\n");
+        exit(1);
+    }
+    int a = block_size;
+    int b;
+    for (size_t i = 0; i < 4; i++)
+    {
+        b= (a>>8);
+        header[2+i]=a-(b<<8);
+        a=b;
+    }
+    FILE *f = fopen(output_path, "w");
+    if (f == NULL)
+    {
+        printf("error in opening the output file: %s\n",output_path);
+        exit(1);
+    }
+    unsigned char* buffer=  malloc(block_size * sizeof(unsigned char));
+    memcpy(buffer,header,54);
+    memcpy(buffer+54,img,(block_size-54)* sizeof(unsigned char));
+    fwrite (buffer , sizeof(unsigned char), block_size, f);
+    fclose(f);
+    free(buffer);
 }
 
 int safe_strtof(float *parameter, char *optarg)
