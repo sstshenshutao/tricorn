56daf6a24b6f236474ff8db8d6c56de3128a1730
Shutao Shen <sstbage@gmail.com>
2020-01-21 17:32:54 +0100

    right version

--- Implementierung/multicornCImp.c
+++ Implementierung/multicornCImp.c
@@ -8,15 +8,21 @@
 #define B_START -1.0
 #define B_END 1.0
 #define ITERATION_NUMBER 70
+
+// check if the value after calculate is convergent
 int is_in_boundary(float a, float b, float r_start, float r_end, float i_start, float i_end, float res)
 {
     return (a > r_start ) && (a < r_end ) && (b > i_start ) && (b < i_end );
 }
+
+// cause of character of float we need to set a accurate count for iteration 
+// that means
+// the value int(x) is only not exact at xxx.999999
+// so we use the following mechanism to avoid it:
+// round_x - x <0.1  Yes? means the value is about x.99999
+// for more detail please see the "ausarbeitung"
 int get_number_x(float x)
 {
-    // the value int(x) is only not exact at xxx.999999
-    // so we use the following mechanism to avoid it:
-    // round_x - x <0.1  Yes? means the value is about x.99999
     int ret = 0;
     int round_x = lrintf(x);
     float diff = round_x - x;
@@ -34,51 +40,45 @@ int get_number_x(float x)
 }
 void multicorn(float r_start, float r_end, float i_start, float i_end, float res, unsigned char *img)
 {
+    //a:real number b:imaginary number
+    //a_number:the for_loop number for a
+    //b_number:the for_loop number for b
     int a_number = get_number_x((A_END - A_START) / res);
     int b_number = get_number_x((B_END - B_START) / res);
     float a = A_START;
     float b = B_START;
     size_t img_index = 0;
-    // firstly raw and the column
-    // printf("debug: a_number %d \n", a_number);
-    // printf("debug: b_number %d \n", b_number);
     float new_a = 0;
     float new_b = 0;
     float tmp = 0;
     int flag = 1;
-    // #debug
-    // int black = 0;
-    // int white = 0;
-    //end debug
+     // firstly raw and then the column
     for (size_t i = 0; i < b_number; i++)
     {
         for (size_t j = 0; j < a_number; j++)
         {
-            //z0=0;
             new_a = 0;
             new_b = 0;
-            // flag = 1;
-            //iteration process
             for (size_t m = 0; m < ITERATION_NUMBER; m++)
             {
+                //for zn = a'+b'i,c = a + b i  
+                //zn+1 = (a'^2 -b'^2 +a)+(-2a'b' +b)i 
                 tmp = new_a;
                 new_a = new_a * new_a - new_b * new_b + a;
                 new_b = -2 * tmp * new_b + b;
             }
+            //the results should be ordered(not NAN, not Inf) and in boundary
+            //if true ==> coloring in black, else ==> coloring in white
             if (isnanf(new_a) || isinff(new_a) || isnanf(new_b) || isinff(new_b) || !is_in_boundary(new_a, new_b, r_start, r_end, i_start, i_end, res))
             {
+                //white
                 img[img_index] = 0xff;
                 img[img_index + 1] = 0xff;
                 img[img_index + 2] = 0xff;
-                // printf("debug white new_a: %f \n", new_a);
-                // printf("debug white new_b: %f \n", new_b);
-                // white++;
-                // flag = 0;
-                // break;
             }
             else
             {
-                // black++;
+                //black
                 img[img_index] = 0x00;
                 img[img_index + 1] = 0x00;
                 img[img_index + 2] = 0x00;
