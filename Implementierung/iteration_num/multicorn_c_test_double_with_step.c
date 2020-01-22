4cc658fa85d5bc190c7152895f7d57d71fd3b37e
Zhiyi Pan <panz@in.tum.de>
2020-01-22 23:14:19 +0100

    Update Implementierung/iteration_num/multicorn_c_test_double_with_step.c

--- Implementierung/iteration_num/multicorn_c_test_double_with_step.c
+++ Implementierung/iteration_num/multicorn_c_test_double_with_step.c
@@ -39,33 +39,23 @@ void multicorn_iteration_number(float r_start, float r_end, float i_start, float
     float a = A_START;
     float b = B_START;
     size_t img_index = 0;
-    // firstly raw and the column
-    // printf("debug: a_number %d \n", a_number);
-    // printf("debug: b_number %d \n", b_number);
     float new_a = 0;
     float new_b = 0;
     float tmp = 0;
     int flag = 1;
-    // #debug
-    // int black = 0;
-    // int white = 0;
-    //end debug
+
     for (size_t i = 0; i < b_number; i++)
     {
         for (size_t j = 0; j < a_number; j++)
         {
-            //z0=0;
+
             new_a = 0;
             new_b = 0;
-            // flag = 1;
+
             //iteration process
             for (size_t m = 0; m < iteration_number; m++)
             {
-                // if (m < 10 && i == 0 && j == 0)
-                // {
-                //     printf("debug_500_a: %f \n", new_a);
-                //     printf("debug_500_b: %f \n", new_b);
-                // }
+    
                 tmp = new_a;
                 new_a = new_a * new_a - new_b * new_b + a;
                 new_b = -2 * tmp * new_b + b;
@@ -74,23 +64,15 @@ void multicorn_iteration_number(float r_start, float r_end, float i_start, float
             {
                 img[img_index] = 0xff;
                 img[img_index + 1] = 0xff;
-                img[img_index + 2] = 0xff;
-                // printf("debug white new_a: %f \n", new_a);
-                // printf("debug white new_b: %f \n", new_b);
-                // white++;
-                // flag = 0;
-                // break;
+  
             }
             else
             {
-                // black++;
+
                 img[img_index] = 0x00;
                 img[img_index + 1] = 0x00;
                 img[img_index + 2] = 0x00;
             }
-            // printf("img_index: %d \n", img_index);
-            // printf("a: %f \n", a);
-            // printf("b: %f \n", b);
             img_index += 3;
             a += res;
         }
@@ -116,11 +98,7 @@ void multicorn_iteration_step(float r_start, float r_end, float i_start, float i
         {
             new_a = last_a[k];
             new_b = last_b[k];
-            // if (i == 0 && j == 0)
-            // {
-            //     printf("debug_new_a: %f \n", new_a);
-            //     printf("debug_new_b: %f \n", new_b);
-            // }
+  
             for (size_t m = 0; m < 1; m++)
             {
                 tmp = new_a;
